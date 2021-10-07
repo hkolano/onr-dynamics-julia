cd1268ec0bc2b6fb8df469f1944d580b3303aed9
hkolano <hkolano@comcast.net>
2021-10-07 15:53:59 -0700

    successfully created a wrench

--- simulate_with_ext_forces.jl
+++ simulate_with_ext_forces.jl
@@ -3,15 +3,17 @@ import RigidBodyDynamics: default_constraint_stabilization_gains
 import RigidBodyDynamics
 import RigidBodyDynamics.cache_eltype
 
-function simulate_with_ext_forces(state0::MechanismState{X}, final_time, control! = zero_torque!;
+function simulate_with_ext_forces(state0::MechanismState{X}, final_time, hydro_calc!, control! = zero_torque!;
         Δt = 1e-4, stabilization_gains=default_constraint_stabilization_gains(X)) where X 
     T = cache_eltype(state0)
     result = DynamicsResult{T}(state0.mechanism)
     control_torques = similar(velocity(state0))
-    closed_loop_dynamics! = let result=result, control_torques=control_torques, stabilization_gains=stabilization_gains # https://github.com/JuliaLang/julia/issues/15276
+    hydro_wrenches = Dict()
+    closed_loop_dynamics! = let result=result, hydro_wrenches=hydro_wrenches, control_torques=control_torques, stabilization_gains=stabilization_gains # https://github.com/JuliaLang/julia/issues/15276
         function (v̇::AbstractArray, ṡ::AbstractArray, t, state)
             control!(control_torques, t, state)
-            dynamics!(result, state, control_torques; stabilization_gains=stabilization_gains)
+            hydro_calc!(hydro_wrenches, t, state)
+            dynamics!(result, state, control_torques, hydro_wrenches; stabilization_gains=stabilization_gains)
             copyto!(v̇, result.v̇)
             copyto!(ṡ, result.ṡ)
             nothing
