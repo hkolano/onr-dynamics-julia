21cd5598c9b5b9654ddcec6cc2922146f7c71eba
hkolano <hkolano@comcast.net>
2022-01-04 16:56:05 -0800

    PD ctlr on base, shoulder, elbow

--- MyCtlr.jl
+++ MyCtlr.jl
@@ -2,9 +2,10 @@ module MyCtlr
 
 using RigidBodyDynamics
 
-Kp = 10.
-Kd = 1.
-
+Kp = 50.
+Kd = 2.0
+torque_lims = (base_joint=9.0, shoulder_joint=9.0, elbow_joint=9.0, wrist_joint=5.0)
+vel_error_cache = (base_joint=[0.0], shoulder_joint=[0.0], elbow_joint=[0.0], wrist_joint=[0.0])
 # ------------------------------------------------------------------------
 #                          SETUP FUNCTIONS
 # ------------------------------------------------------------------------
@@ -16,7 +17,7 @@ Kd = 1.
     end
 
     function set_ctlr_time_step(time_step)
-        dt = time_step
+        global dt = time_step
     end
 
     function resettimestep()
@@ -52,24 +53,27 @@ Kd = 1.
 function simple_control!(torques::AbstractVector, t, state_alpha::MechanismState, hydro_wrenches::Dict{BodyID, Wrench{Float64}})
     # If it's the first time called in the Runge-Kutta, update the control torque
     if rem(time_step_ctr, 4) == 0
-        println("New control goes here!!!")
+        # println("New control goes here!!!")
 
         # Get the desired velocity values for each joint
         get_des_vel!(des_vel, t)
 
-        tau_base = MyCtlr.base_P_ctlr(torques[velocity_range(state_alpha, base_joint)][1], t, velocity(state_alpha, base_joint), base_joint, des_vel)
-    
+        tau_base = MyCtlr.PD_ctlr(torques[velocity_range(state_alpha, base_joint)][1], t, velocity(state_alpha, base_joint), des_vel[base_joint], 1)
+        tau_shoulder = MyCtlr.PD_ctlr(torques[velocity_range(state_alpha, shoulder_joint)][1], t, velocity(state_alpha, shoulder_joint), des_vel[shoulder_joint], 2)
+        tau_elbow = MyCtlr.PD_ctlr(torques[velocity_range(state_alpha, elbow_joint)][1], t, velocity(state_alpha, elbow_joint), des_vel[elbow_joint], 3)
+        tau_wrist = MyCtlr.PD_ctlr(torques[velocity_range(state_alpha, wrist_joint)][1], t, velocity(state_alpha, wrist_joint), des_vel[wrist_joint], 4)
+
         torques[velocity_range(state_alpha, base_joint)] .= tau_base
-        torques[velocity_range(state_alpha, shoulder_joint)] .= 0 #tau[shoulder_joint][1]
-        torques[velocity_range(state_alpha, elbow_joint)] .= 0.0 #tau[elbow_joint][1]
-        torques[velocity_range(state_alpha, wrist_joint)] .= 0.0 #tau[wrist_joint][1]
+        torques[velocity_range(state_alpha, shoulder_joint)] .= tau_shoulder #tau[shoulder_joint][1]
+        torques[velocity_range(state_alpha, elbow_joint)] .= tau_elbow #0.0 #tau[elbow_joint][1]
+        torques[velocity_range(state_alpha, wrist_joint)] .= tau_wrist #tau[wrist_joint][1]
     end
 
     # push!(ctlr_taus, tau_base)
     # push!(times, t)
 
     global time_step_ctr += 1
-    println("control torques: ", torques)
+    # println("Base joint vel errors: ", vel_error_cache.base_joint)
 end;
 
 function get_des_vel!(des_vel, t)
@@ -80,27 +84,25 @@ function get_des_vel!(des_vel, t)
     des_vel[wrist_joint][1] = 0.00
 end;
     
-function base_P_ctlr(torque, t, vel_act, joint_name, des_vel)
-    # println("current time step: ", time_step_ctr)
-    # println("remainder after div 4: ", rem(time_step_ctr, 4))
-    # println(des_vel[joint_name][1])
-    # println("vel_act: ", vel_act)
-    vel_error = vel_act[1] - des_vel[joint_name][1]
-    d_tau = -Kp*vel_error
-
-    println("At t = ", t, ", actual vel = ", vel_act)
+function PD_ctlr(torque, t, vel_act, des_vel, j_idx)
+    vel_error = vel_act[1] - des_vel[1]
+    d_vel_error = (vel_error - last(vel_error_cache[j_idx]))/dt
+    d_tau = -Kp*vel_error - Kd*d_vel_error
 
     # Can only change torque a small amount per time step
-    if d_tau < -.1
-        d_tau = -0.1
-    elseif d_tau > 0.1
-        d_tau = 0.1
+    if d_tau < -.05
+        d_tau = -0.05
+    elseif d_tau > 0.05
+        d_tau = 0.05
     end
     
     # Torque limits
     new_tau = torque .+ d_tau
-    impose_torque_limit!(new_tau, 9.0)
-    
+    impose_torque_limit!(new_tau, torque_lims[j_idx])
+
+    # store velocity error term
+    push!(vel_error_cache[j_idx], vel_error)
+
     return new_tau
 end
 
